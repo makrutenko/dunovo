d17e5203354382430bf9751365a8b5fec9704272
Nick <nmapsy@gmail.com>
2022-02-16 13:46:11 -0500

    conda: Add Conda build updates and improvements suggested by Bioconda team.

--- conda/build.sh
+++ conda/build.sh
@@ -1,34 +1,16 @@
-
-# Download submodules and move them to lib.
-
-cat > submodules.txt <<EOF
-kalign  0.3.0       makovalab-psu kalign-dunovo c0ef2de4a958aed47311ea86591debb3bada871143a6923bc6338ab2f99f2d5b
-utillib 0.1.1-alpha NickSto       utillib       961f5a3481d1c0dbe00c258b9df2de541e5605f3de4ff25bcc3cec22922e7c06
-ET      0.3         NickSto       ET            6b757b3ab3634b949f78692ac0db72e9264a1dceaf7449e921091d3ad0012eea
-bfx     0.5.0       NickSto       bfx           f5cc5c9c8a627a082f8b505135aec10b47c09647269ad92abe3a1fa2f2059048
-EOF
-
-while read name version owner repo hash; do
-  echo "Downloading $name v$version from $owner/$repo"
-  wget --no-check-certificate "https://github.com/$owner/$repo/archive/v$version.tar.gz"
-  downloaded_hash=`sha256sum "v$version.tar.gz" | tr -s ' ' | cut -d ' ' -f 1`
-  if ! [ "$hash" == "$downloaded_hash" ]; then
-    echo "Error: Hash does not match!" >&2
-    return 1
-  fi
-  tar -zxvpf "v$version.tar.gz"
-  rm -rf "$name"
-  mv "$repo-$version" "$name"
-  rm "v$version.tar.gz"
-done < submodules.txt
+SubModules="bfx kalign utillib ET"
 
 # Compile binaries and move them to lib.
-make
+if [ "x$CC" = x ]; then
+  make
+else
+  make "CC=$CC"
+fi
 mkdir -p "$PREFIX/lib"
 mv *.so "$PREFIX/lib"
-while read name rest; do
-  mv "$name" "$PREFIX/lib/$name"
-done < submodules.txt
+for submodule in $SubModules; do
+  mv "$submodule" "$PREFIX/lib/$submodule"
+done
 
 # Move scripts to lib and link to them from bin.
 mkdir -p "$PREFIX/bin"
